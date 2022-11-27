5450cdaf64648cd7da716b9c9b95571518be003a
R. Fariz <farizramazanov38@gmail.com>
2022-11-27 19:27:26 +0400

    feat: fix image width bug

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,11 +1,13 @@
-import React from 'react'
+
+import './Profile.scss'
+
 import MainProfile from './mainProfile/MainProfile';
 import MainProfileImg from "../../resources/img/mainProfile.jpg";
 
 
 const Profile = () => {
   return (
-    <main>
+    <main className='profile'>
       <img src={MainProfileImg} alt="header image" />
       <MainProfile />
     </main>
